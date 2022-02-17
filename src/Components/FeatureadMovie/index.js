6c8200d650af93a0d06e5586f94365f6f85af846
amanda passos <amanda_passos@outlook.com>
2022-02-17 16:33:43 -0300

    atualizações

---
+++ src/Components/FeatureadMovie/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function featuread () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default featuread;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
 import React , { useEffect, useState } from 'react';
+import '../Components/MovieRow/index.css';
 import Tmdb from '../Tmdb/index.js';
-import ListMovie from '../Components/MovieRow/ListMovie';
+import "../App.css";
+import ListMovie from '../Components/MovieRow/ListMovie.js';
+import Featuread from '../Components/FeatureadMovie/index.js';
 
 function Home() {
 
     const [ movieList, setMovieList ] = useState([]);
-
+    const [ featureadData, setFeatureadData ] = useState(null);
 
     useEffect(() => {
         const loadAll = async () => {
             const list = await Tmdb.homeList();
             setMovieList(list);
+
+            const originals = list.filter(i=>i.slug === 'originals');
+            const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+            const chosen = originals[0].items.results[randomChosen];
+
         }
 
         loadAll();
@@ -18,13 +26,18 @@ function Home() {
 
     return (
         <div className='page'>
+
+            { featureadData &&
+                <Featuread item={featureadData} />
+            }
+
             <section className='lists'>
                 {movieList.map((item, key) => (
-                    <ListMovie key={key}/>
+                    <ListMovie key={key} title={item.title} items={item.items}/>
                 ))}
             </section>
         </div>
     );
 }
 
-export default Home();
+export default Home;
