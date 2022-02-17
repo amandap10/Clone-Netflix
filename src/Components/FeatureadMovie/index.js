f5eeb34683d5a3ac7598f78c6cd02ea0c7f86c04
amanda passos <amanda_passos@outlook.com>
2022-02-17 19:57:26 -0300

    info

--- src/Components/FeatureadMovie/index.js
+++ src/Components/FeatureadMovie/index.js
@@ -2,9 +2,41 @@ import React from 'react';
 import './index.css';
 
 function featuread ({item}) {
+
+    const firstDate = new Date(item.first_air_date);
+
     return (
-       <section>
-           <div>{item.original_name}</div>
+       <section className="featured" style={{
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+            }}>
+           <div className="featured-vertical">
+               <div className="featured-horizontal">
+                   <div className="featured-name">
+                       {item.original_name}
+                   </div>
+                   <div className="featured-info">
+                       <div className="featured-points">
+                           {item.vote_average}
+                       </div>
+                       <div className="featured-yar">
+                           {firstDate.getFullYear()}
+                       </div>
+                       <div className="featured-seasons">
+                           {item.number_of_seasons} Temporada
+                           {item.number_of_seasons !== 1 ? 's' : ''}
+                       </div>
+                   </div>
+                   <div className="featured-description">
+                       {item.overview}
+                   </div>
+                   <div className="featured-buttons">
+
+                   </div>
+                   <div className="featured-genres">
+                       <strong>Gêneros</strong> ...
+                   </div>
+               </div>
+            </div>
        </section>
     )
 }
