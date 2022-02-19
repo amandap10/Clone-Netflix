42578fad0a31277b21745aab23f918b5afabc4df
amanda passos <amanda_passos@outlook.com>
2022-02-19 20:07:12 -0300

    detalhes

--- src/Components/MovieRow/ListMovie.js
+++ src/Components/MovieRow/ListMovie.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./index.css";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export default ({title, items}) => {
     return (
         <div className="movieRow">
            <h2>{title}</h2>
-            <div className=""></div>
+            <div className="movieRow-left">
+                <ArrowBackIosNewIcon/>
+            </div>
+            <div className="movieRow-right">
+                <ArrowForwardIosIcon />
+            </div>
 
            <div className="movieRow-listarea">
                <div className="movieRow-list">
