7f40701dcab52c76f9883aa7394098ce926a8e43
amanda passos <amanda_passos@outlook.com>
2022-02-20 16:51:49 -0300

    processo de autenticação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+
+import './Services/firebase';
 
 
 function App() {
